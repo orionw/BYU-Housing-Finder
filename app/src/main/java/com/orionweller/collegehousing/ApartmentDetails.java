ba0acb3148c5466ce909a314d97c3ccba3e80736
orionw <orionjamesweller@gmail.com>
2018-06-30 23:44:33 -0600

    App downloads images for each different apartment and displays them. Possible memory problems.

--- app/src/main/java/com/orionweller/collegehousing/ApartmentDetails.java
+++ app/src/main/java/com/orionweller/collegehousing/ApartmentDetails.java
@@ -1,87 +1,263 @@
 package com.orionweller.collegehousing;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 
 public class ApartmentDetails extends Activity {
 
-    private String imageUrls[] = {"https://cdn.pixabay.com/photo/2018/06/10/17/40/olives-3466908__340.jpg",
-            "https://cdn.pixabay.com/photo/2018/06/24/03/06/ship-3493887__340.jpg",
-            "http://www.abc.com/image3.jpg"
-    };
+    private Context mContext;
+    private Activity mActivity;
 
-    private class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
-        ImageView bmImage;
+    private CoordinatorLayout mCLayout;
+    private Button mButtonDo;
+    private ProgressDialog mProgressDialog;
+    private LinearLayout mLLayout;
 
-        public ImageDownloader(ImageView bmImage) {
-            this.bmImage = bmImage;
-        }
+    private AsyncTask mMyTask;
 
-        protected Bitmap doInBackground(String... urls) {
-            String url = urls[0];
-            Bitmap bitmap = null;
-            try {
-                InputStream in = new java.net.URL(url).openStream();
-                bitmap = BitmapFactory.decodeStream(in);
-            } catch (Exception e) {
-                Log.e("MyApp", e.getMessage());
-            }
-            return bitmap;
-        }
+    // Specify some url to download images
+    final URL url1 = stringToURL("http://www.freeimageslive.com/galleries/transtech/informationtechnology/pics/beige_keyboard.jpg");
+    final URL url2 = stringToURL("http://www.freeimageslive.com/galleries/transtech/informationtechnology/pics/computer_blank_screen.jpg");
+    final URL url3 = stringToURL("http://www.freeimageslive.com/galleries/transtech/informationtechnology/pics/computer_memory_dimm.jpg");
+    final URL url4 = stringToURL("http://www.freeimageslive.com/galleries/transtech/informationtechnology/pics/computer_memory.jpg");
+    final URL url5 = stringToURL("http://www.freeimageslive.com/galleries/transtech/informationtechnology/pics/ethernet_router.jpg");
+    public static List<Bitmap> images;
+    ViewPager viewPager;
+    MyPagerAdapter myPagerAdapter;
 
-        protected void onPostExecute(Bitmap result) {
-            bmImage.setImageBitmap(result);
-        }
-    }
+    public static boolean hasLoaded = false;
+    public static String lastApartment;
 
-    private int currImage = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.apartment_details);
+        if (lastApartment == null) {
+            lastApartment = "";
+        }
 
         Intent intent = getIntent();
         String apartmentName;
         apartmentName = intent.getExtras().getString("apartmentName");
-        Log.v("apartmentName", apartmentName);
+        if (!lastApartment.equals(apartmentName)) {
+            hasLoaded = false;
+            lastApartment = apartmentName;
+        }
 
-        setInitialImage();
-        setImageRotateListener();
-    }
+        if (!hasLoaded) {
+            setContentView(R.layout.details_start);
 
-    private void setImageRotateListener() {
-        final Button rotatebutton = (Button) findViewById(R.id.btnRotateImage);
-        rotatebutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                currImage++;
-                if (currImage == 3) {
-                    currImage = 0;
+            // Get the application context
+            mContext = getApplicationContext();
+            mActivity = ApartmentDetails.this;
+            mLLayout = (LinearLayout) findViewById(R.id.detail_linear);
+
+            // Initialize the progress dialog
+            mProgressDialog = new ProgressDialog(mActivity);
+            mProgressDialog.setIndeterminate(false);
+            // Progress dialog horizontal style
+            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            // Progress dialog title
+            mProgressDialog.setTitle("Getting images");
+            // Progress dialog message
+            mProgressDialog.setMessage("Please wait, we are downloading the apartment's photos...");
+            mProgressDialog.setCancelable(true);
+            mMyTask = new DownloadTask()
+                    .execute(
+                            url1,
+                            url2,
+                            url3,
+                            url4,
+                            url5
+                    );
+            // Set a progress dialog dismiss listener
+            mProgressDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                @Override
+                public void onDismiss(DialogInterface dialogInterface) {
+                    // Cancel the AsyncTask
+                    mMyTask.cancel(false);
                 }
-                setCurrentImage();
+            });
+        } else {
+            setContentView(R.layout.apartment_details);
+            viewPager = (ViewPager) findViewById(R.id.viewPager);
+            myPagerAdapter = new MyPagerAdapter(ApartmentDetails.this, images);
+            viewPager.setAdapter(myPagerAdapter);
+            mLLayout = (LinearLayout) findViewById(R.id.real_linear_details);
+
+        }
+    }
+
+        // Get the widget reference from XML layout
+//        mCLayout = (CoordinatorLayout) findViewById(R.id.coordinator_layout);
+//        mButtonDo = (Button) findViewById(R.id.btn_do);
+//        mLLayout = (LinearLayout) findViewById(R.id.ll);
+
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mProgressDialog != null) {
+            mProgressDialog.dismiss();
+            mProgressDialog = null;
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mProgressDialog != null) {
+            mProgressDialog.dismiss();
+            mProgressDialog = null;
+        }
+    }
+
+
+//        // Initialize a new click listener for positive button widget
+//        mButtonDo.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // Execute the async task
+//                // Don't know what they want
+//            }
+//        });
+//    }
+    /*
+        First parameter URL for doInBackground
+        Second parameter Integer for onProgressUpdate
+        Third parameter List<Bitmap> for onPostExecute
+     */
+    private class DownloadTask extends AsyncTask<URL, Integer, List<Bitmap>> {
+        // Before the tasks execution
+        protected void onPreExecute() {
+            // Display the progress dialog on async task start
+            mProgressDialog.show();
+            mProgressDialog.setProgress(0);
+        }
+
+        // Do the task in background/non UI thread
+        protected List<Bitmap> doInBackground(URL... urls) {
+            int count = urls.length;
+            //URL url = urls[0];
+            HttpURLConnection connection = null;
+            List<Bitmap> bitmaps = new ArrayList<>();
+
+            // Loop through the urls
+            for (int i = 0; i < count; i++) {
+                URL currentURL = urls[i];
+                // So download the image from this url
+                try {
+                    // Initialize a new http url connection
+                    connection = (HttpURLConnection) currentURL.openConnection();
+
+                    // Connect the http url connection
+                    connection.connect();
+
+                    // Get the input stream from http url connection
+                    InputStream inputStream = connection.getInputStream();
+
+                    // Initialize a new BufferedInputStream from InputStream
+                    BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+
+                    // Convert BufferedInputStream to Bitmap object
+                    Bitmap bmp = BitmapFactory.decodeStream(bufferedInputStream);
+
+                    // Add the bitmap to list
+                    bitmaps.add(bmp);
+
+                    // Publish the async task progress
+                    // Added 1, because index start from 0
+                    publishProgress((int) (((i + 1) / (float) count) * 100));
+                    if (isCancelled()) {
+                        break;
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    // Disconnect the http url connection
+                    connection.disconnect();
+                }
             }
-        });
+            // Return bitmap list
+            Log.d(TAG, bitmaps.toString());
+            return bitmaps;
+        }
+
+        // On progress update
+        protected void onProgressUpdate(Integer... progress) {
+            // Update the progress bar
+            mProgressDialog.setProgress(progress[0]);
+        }
+
+        // On AsyncTask cancelled
+        protected void onCancelled() {
+            mProgressDialog.dismiss();
+            mProgressDialog = null;
+            Snackbar.make(mCLayout, "Task Cancelled.", Snackbar.LENGTH_LONG).show();
+        }
+
+        // When all async task done
+        protected void onPostExecute(List<Bitmap> result) {
+            // Hide the progress dialog
+            mProgressDialog.dismiss();
+            mProgressDialog = null;
+
+            images = result;
+
+            setContentView(R.layout.apartment_details);
+            viewPager = (ViewPager)findViewById(R.id.viewPager);
+            myPagerAdapter = new MyPagerAdapter(ApartmentDetails.this, images);
+            viewPager.setAdapter(myPagerAdapter);
+            hasLoaded = true;
+            mLLayout = (LinearLayout) findViewById(R.id.real_linear_details);
+
+
+            // Remove all views from linear layout
+            //mLLayout.removeAllViews();
+
+        }
     }
 
-    private void setInitialImage() {
-        setCurrentImage();
-    }
 
-    private void setCurrentImage() {
-        final ImageView imageView = (ImageView) findViewById(R.id.imageDisplay);
-        ImageDownloader imageDownLoader = new ImageDownloader(imageView);
-        imageDownLoader.execute(imageUrls[currImage]);
+
+
+    // Custom method to convert string to url
+    protected URL stringToURL(String urlString) {
+        try {
+            URL url = new URL(urlString);
+            return url;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
---
+++ app/src/main/java/com/orionweller/collegehousing/MyPagerAdapter.java
@@ -0,0 +1,67 @@
+package com.orionweller.collegehousing;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MyPagerAdapter extends PagerAdapter{
+    Context context;
+    List<Bitmap> images;
+    LayoutInflater layoutInflater;
+
+
+    public MyPagerAdapter(Context context, List<Bitmap> images) {
+        this.context = context;
+        this.images = images;
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        if (images != null) {
+            return images.size();
+
+        }
+        else {
+            return 0;
+        }
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((LinearLayout) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, final int position) {
+        View itemView = layoutInflater.inflate(R.layout.item, container, false);
+
+        ImageView imageView = (ImageView) itemView.findViewById(R.id.imageView);
+        imageView.setImageBitmap(images.get(position));
+
+        container.addView(itemView);
+
+        //listening to image click
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "you clicked image " + (position + 1), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return itemView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((LinearLayout) object);
+    }
+}
